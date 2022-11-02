5c7ac9fb765803b9e0222cd4187103d35812848b
KuroTori <1141759094@qq.com>
2022-11-01 21:37:08 -0400

    Navigation

---
+++ Assets/Scripts/MapNavi.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapNavi : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+    public Transform startPos;
+    public Transform endPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0, startPos.position);
+        lineRenderer.SetPosition(1, endPos.position);
+    }
+}
